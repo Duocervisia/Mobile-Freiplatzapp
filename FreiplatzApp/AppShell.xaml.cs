656f52c86695b782e5b6160e7fed18d9f9caed8c
Tobias <Tobias.Henning@student.htw-berlin.de>
2021-10-25 17:06:20 +0200

    Projektdateien hinzufügen.

---
+++ FreiplatzApp/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using FreiplatzApp.ViewModels;
+using FreiplatzApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace FreiplatzApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
