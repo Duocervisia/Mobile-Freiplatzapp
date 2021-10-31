9578bc525426bfa6a601ca0772be8010536f5bc8
Tobias <tobias.henning@student.htw-berlin.de>
2021-10-31 17:22:49 +0100

    Added Enums and LocationEntry

---
+++ FreiplatzApp/Models/Enums.cs
@@ -0,0 +1,17 @@
+using Sytem;
+
+public class Enums
+{
+    //Paragraphs from SGB 8 (Sozailgesetzbuch 8)
+    enum Paragraphs
+    {
+        19 = "Gemeinsame Wohnformen für Mütter/Väter und KInder" ,
+        27 = "Hilfe zur Erziehung",
+        28 = "Erziehungsberatung",
+        29 = "Soziale Gruppenarbeit",
+
+        
+        34 = "Heimerziehung, sonstige betreute Wohnform", 
+        41 = "Hilfe für junge Volljährige"
+    }
+}
---
+++ FreiplatzApp/Models/LocationEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FreiplatzApp.Models
+{
+    public class LocationEntry
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int MinAge { get; set; }
+        public int MaxAge { get; set; }
+        public string
+
+    }
+}
