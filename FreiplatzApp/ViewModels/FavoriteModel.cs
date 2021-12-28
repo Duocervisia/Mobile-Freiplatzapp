efdc9c83be4359ed3fa6a4b0b96a052e9caac162
Fenia Franziska Grunert <fenia.grunert@htw-berlin.de>
2021-12-28 15:19:40 +0100

    start Favorite

---
+++ FreiplatzApp/ViewModels/FavoriteModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using FreiplatzApp.Models;
+using System.Threading.Tasks;
+
+
+namespace FreiplatzApp.ViewModels
+{
+    public class FavoriteModel : ViewModelBase
+    {
+        public FavoriteModel()
+        {
+            
+        }
+    }
+}
